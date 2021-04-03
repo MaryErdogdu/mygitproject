022c338b5c334e86a4d7d3de5e12b43f45b5619a
MaryErdogdu <erdogdu.mary@gmail.com>
2021-04-03 12:54:33 -0400

    version3

--- src/mygitpackage/mygitclass.java
+++ src/mygitpackage/mygitclass.java
@@ -4,6 +4,8 @@ public class mygitclass {
 
 	public static void main(String[] args) {
 		System.out.println("Version 1");
+		System.out.println("version 2");
+		System.out.println("version 3");
 		
 	}
 
